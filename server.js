8f4321aeebf0c42fc9998f4ea45c2f9f989fc6a5
Fredrik <Fredrik@n152-p115.kthopen.kth.se>
2015-01-16 14:26:40 +0100

    server fix

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 3000;
 app.use(express.static(__dirname + '/public'));
-app.listen(3000, function() {
-  console.log('Listening on 3000');
+app.listen(port, function() {
+  console.log('Listening on '+port);
 }); 
